48e14c06b77a65a1805710bd18a9c186acd0889a
ot0527 <t.ogasawara.0527@gmail.com>
2025-06-23 17:06:44 +0900

    数独ver1.0

---
+++ script.js
@@ -0,0 +1,24 @@
+const createBoard = () => {
+  const boardId = document.getElementById("Board");
+  for (let i = 0; i < 9; i++) {
+    const tr = document.createElement("tr");
+    for (let j = 0; j < 9; j++) {
+      const td = document.createElement("td");
+      const input = document.createElement("input");
+      input.type = "text";
+      input.maxLength = 1;
+      input.oninput = function () {
+        validateInput(this);
+      };
+      input.classList.add("cell");
+      td.appendChild(input);
+      tr.appendChild(td);
+    }
+    boardId.appendChild(tr);
+  }
+
+  function validateInput(el) {
+    el.value = el.value.replace(/[^0-9]/g, "");
+  }
+};
+createBoard();
